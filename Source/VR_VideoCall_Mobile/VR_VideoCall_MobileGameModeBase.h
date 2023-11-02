48110e447efd26a77a1a8f4a1159fd59abb21d4d
AbolfazlMoghanlu <Abolfazlmoghanlu@gmail.com>
2023-11-02 16:45:34 +0330

    update package maps

--- Source/VR_VideoCall_Mobile/VR_VideoCall_MobileGameModeBase.h
+++ Source/VR_VideoCall_Mobile/VR_VideoCall_MobileGameModeBase.h
@@ -4,7 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+//#include ""
+
+// #if PLATFORM_ANDROID
+// #include "AndroidPermission/Classes/AndroidPermissionFunctionLibrary.h"
+// #endif
+
 #include "VR_VideoCall_MobileGameModeBase.generated.h"
+
+// using namespace agora::rtc;
+// using namespace agora;
 
 /**
  * 
@@ -14,6 +23,26 @@ class VR_VIDEOCALL_MOBILE_API AVR_VideoCall_MobileGameModeBase : public AGameMod
 {
 	GENERATED_BODY()
 
+public:
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	virtual void BeginPlay() override;
+
+
 	void InitAgoraWidget(FString APP_ID, FString TOKEN, FString CHANNEL_NAME);
 	
+	//void CheckPermission();
+
+	//void InitAgoraEngine(FString APP_ID, FString TOKEN, FString CHANNEL_NAME);
+
+protected:
+
+// 	IRtcEngine* RtcEngineProxy;
+// 
+// 	FString AppId;
+// 	FString Token;
+// 	FString ChannelName;
+// 
+// 	TSharedPtr<FUserRtcEventHandler> UserRtcEventHandler;
 };
